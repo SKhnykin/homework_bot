81514c296b7b89d732c66677506fe0083146cfad
Stanislav Khnykin <stas.kh@inbox.ru>
2022-05-14 10:52:32 +0300

    Первые наброски функций

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import requests
+import logging
+import os
+import sys
+import time
+import telegram
+
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,19 +27,33 @@ HOMEWORK_STATUSES = {
 }
 
 
+logger = logging.getLogger(__name__)
+handler = RotatingFileHandler(
+    'my_bot.log',
+    maxBytes=1048576,
+    backupCount=10
+)
+logger.addHandler(handler)
+
+
 def send_message(bot, message):
-    ...
+    """Функция отправки сообщения в Телеграм."""
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
-def get_api_answer(current_timestamp):
+def get_api_answer(current_timestamp) -> dict:
+    """Делает запрос к API-сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if homework_statuses.status_code != requests.codes.ok:
+        return {}
+    else:
+        return homework_statuses.json()
 
 
 def check_response(response):
-
+    """Проверяем ответ API на корректность."""
     ...
 
 
@@ -48,12 +70,18 @@ def parse_status(homework):
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+def check_tokens() -> bool:
+    """Функция проверяющая доступность переменных окружения."""
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        return True
+    else:
+        return False
 
 
 def main():
     """Основная логика работы бота."""
+    if not check_tokens():
+        sys.exit(1)
 
     ...
 
@@ -68,7 +96,7 @@ def main():
 
             ...
 
-            current_timestamp = ...
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
